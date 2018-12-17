8d07722e4de88b88fff2760479762823e3fc2ddc
Pierce Darragh <pierce.darragh@gmail.com>
2018-12-16 18:53:59 -0700

    Create JSON typealias

--- Sources/ScrollRack/Handlers/HandlerCommon.swift
+++ Sources/ScrollRack/Handlers/HandlerCommon.swift
@@ -7,15 +7,17 @@
 
 import PerfectHTTP
 
-private func encodeJson(_ json: [String: Any]) -> String {
+private func encodeJson(_ json: JSON) -> String {
     guard let responseString = try? json.jsonEncodedString() else {
         fatalError("Could not encode JSON dictionary.")
     }
     return responseString
 }
 
-func makeJsonResponseBody(fromDictionary dict: [String: Any], withResponse response: HTTPResponse) {
+func makeJsonResponseBody(fromDictionary dict: JSON, withResponse response: HTTPResponse) {
     response.setHeader(.contentType, value: "application/json")
     response.setBody(string: encodeJson(dict))
     response.completed()
 }
+
+typealias JSON = [String: Any]
