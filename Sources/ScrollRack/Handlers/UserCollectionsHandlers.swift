198a5802c6861aa844d68e3d8f185aa429d7ba06
Pierce Darragh <pierce.darragh@gmail.com>
2018-12-16 21:31:45 -0700

    Add remaining /users/{id}/collections/{id} endpoints

--- Sources/ScrollRack/Handlers/UserCollectionsHandlers.swift
+++ Sources/ScrollRack/Handlers/UserCollectionsHandlers.swift
@@ -40,10 +40,36 @@ private func getUserCollectionHandler(request: HTTPRequest, response: HTTPRespon
     makeJsonResponseBody(fromDictionary: responseDict, withResponse: response)
 }
 
+private func putUserCollectionHandler(request: HTTPRequest, response: HTTPResponse) -> () {
+    let user_id = extractUrlVariable("user_id", fromRequest: request)
+    let collection_id = extractUrlVariable("collection_id", fromRequest: request)
+
+    let responseDict = [
+        "user_id": "\(user_id)",
+        "collection_id": "\(collection_id)",
+    ]
+
+    makeJsonResponseBody(fromDictionary: responseDict, withResponse: response)
+}
+
+private func deleteUserCollectionHandler(request: HTTPRequest, response: HTTPResponse) -> () {
+    let user_id = extractUrlVariable("user_id", fromRequest: request)
+    let collection_id = extractUrlVariable("collection_id", fromRequest: request)
+
+    let responseDict = [
+        "user_id": "\(user_id)",
+        "collection_id": "\(collection_id)",
+    ]
+
+    makeJsonResponseBody(fromDictionary: responseDict, withResponse: response)
+}
+
 func getUserCollectionsRoutes() -> Routes {
     var routes = Routes()
     routes.add(method: .get, uri: "/users/{user_id}/collections", handler: getUserCollectionsHandler)
     routes.add(method: .post, uri: "/users/{user_id}/collections", handler: postUserCollectionsHandler)
     routes.add(method: .get, uri: "/users/{user_id}/collections/{collection_id}", handler: getUserCollectionHandler)
+    routes.add(method: .put, uri: "/users/{user_id}/collections/{collection_id}", handler: putUserCollectionHandler)
+    routes.add(method: .delete, uri: "/users/{user_id}/collections/{collection_id}", handler: deleteUserCollectionHandler)
     return routes
 }
