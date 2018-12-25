d30ace7a1d742dd42d040b3ed42ee8cafbdc19a8
Pierce Darragh <pierce.darragh@gmail.com>
2018-12-24 23:50:38 -0700

    Add vapor/auth SPM dependency

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ let package = Package(
     name: "ScrollRack",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor"]),
+        .target(name: "App", dependencies: ["Authentication", "FluentMySQL", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
