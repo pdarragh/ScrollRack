bc4e71ec1bcecb69f144aa8a24cf431b30219b6f
Pierce Darragh <pierce.darragh@gmail.com>
2019-01-17 13:21:16 -0700

    Attempt to configure custom JSON en/decoder

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -13,6 +13,17 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
+    /// FIXME: This doesn't appear to do anything.
+    /// Configure content encoder/decoder.
+    var content = ContentConfig.default()
+    let encoder = JSONEncoder()
+    encoder.keyEncodingStrategy = .convertToSnakeCase
+    content.use(dataEncoder: encoder, for: .json)
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    content.use(dataDecoder: decoder, for: .json)
+    services.register(content)
+
     /// Register routes to the router.
     let router = EngineRouter.default()
     try buildRoutesForRouter(router)
